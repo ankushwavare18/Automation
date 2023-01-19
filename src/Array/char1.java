81f9a178b5d71fc89a42e820e1655a7e7a74a774
Admin <Admin@Admin-PC>
2023-01-19 18:54:36 +0530

    Hello

---
+++ src/Array/char1.java
@@ -0,0 +1,36 @@
+package Array;
+
+public class char1 
+{
+  public static void main(String[] args) 
+  {
+	//Array declaration
+	  char ar2[]=new char[4];
+	  
+	  //Array initialization
+	  ar2[0]='A';
+	  ar2[1]='B';
+	  ar2[2]='C';
+	  ar2[3]='D';
+	  
+	  //Usage
+	  System.out.println(ar2[0]);
+	  System.out.println(ar2[1]);
+	  System.out.println(ar2[2]);
+	  System.out.println(ar2[3]);
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
